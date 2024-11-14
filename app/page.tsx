6373eaaf815196aa8039c97a33c70776cfac31cc
Kawa_ <yuyux0910@gmail.com>
2024-11-15 00:01:13 +0900

    [delete] 不要な記述を削除
    layout.tsxに記述したため不要となったfaviconの設定部分を削除した

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main>
-      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
       <div> 
         <div>
           <h1>
