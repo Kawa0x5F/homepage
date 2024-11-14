39933098b4fa92e4d35f0c90bc0a12bb0987ce02
Kawa_ <yuyux0910@gmail.com>
2024-11-14 23:41:57 +0900

    [change,delete] 画像の位置を変更，カスタムドキュメントファイルを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Head from 'next/head'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Kawa_Web',
+  description: 'Kawa_のホームページです.',
+}
 
 export default function Home() {
   return (
     <main>
-      <Head>
-        <title>Kawa_Web</title>
-        <meta name="description" content="Kawa_のホームページです."></meta>
-      </Head>
-
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <div> 
         <div>
           <h1>
