43a3fa9b97478bb882264b792a6785434e9e1805
Kawa_ <yuyux0910@gmail.com>
2025-02-22 15:47:16 +0900

    [add] 色を茶色に変更

--- app/component/BackgroundText.tsx
+++ app/component/BackgroundText.tsx
@@ -1,16 +1,15 @@
 export default function BackgroundText() {
-    return (
-      <div className="fixed bottom-8 right-8 w-auto h-auto flex items-end pointer-events-none">
-        <p
-          className="font-bold text-gray-300 opacity-20 leading-none whitespace-nowrap 
-                     text-8xl sm:text-10xl md:text-[12rem] lg:text-[15rem] xl:text-[20rem]"
-          style={{
-            fontSize: "clamp(8rem, 20vw, 30rem)", // 画面幅の 20% を基準に拡大
-          }}
-        >
-          Kawa_
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="fixed bottom-8 right-8 w-auto h-auto flex items-end pointer-events-none">
+      <p
+        className="font-bold text-[#d2b48c] opacity-20 leading-none whitespace-nowrap 
+                   text-8xl sm:text-10xl md:text-[12rem] lg:text-[15rem] xl:text-[20rem]"
+        style={{
+          fontSize: "clamp(8rem, 20vw, 30rem)", // 画面幅の 20% を基準に拡大
+        }}
+      >
+        Kawa_
+      </p>
+    </div>
+  );
+}
