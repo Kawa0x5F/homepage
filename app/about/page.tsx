137383642981481c8e067e57508a960c5da75803
kawa0x5f <yuyux0910@gmail.com>
2025-03-26 22:16:54 +0900

    refact: metadataを更新

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import './../ui/globals.css'
 
 export const metadata: Metadata = {
-  title: 'Kawa_Web',
+  title: 'Kawa_About',
   description: 'Kawa_のホームページです.',
 }
 
